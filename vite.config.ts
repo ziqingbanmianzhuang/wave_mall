e7da976368cf970a37c34d41e3d558e867d4cbf6
ziqing <1721370034@qq.com>
2024-02-02 17:41:12 +0800

    安装VueDevTools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
+import VueDevTools from 'vite-plugin-vue-devtools'
 import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+	plugins: [uni(), VueDevTools()],
 });
