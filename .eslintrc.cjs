fa5380c8d998756195173d2696fb10afd71c33ec
ziqing <1721370034@qq.com>
2024-02-03 16:55:23 +0800

    更新package.json的Lint-staged配置,提交前运行eslint及prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,18 @@
 // require('@rushstack/eslint-patch/modern-module-resolution')
 require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
-	root: true,
-	extends: [
-		"plugin:vue/vue3-recommended",
-		"eslint:recommended",
-		"@vue/eslint-config-typescript",
-		"prettier",
-	],
-	parserOptions: {
-		ecmaVersion: "latest",
-	},
-	rules: {
-		"vue/multi-word-component-names": ["warn"],
-		"@typescript-eslint/no-unused-vars": ["warn"],
-	},
+  root: true,
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/multi-word-component-names": ["warn"],
+    "@typescript-eslint/no-unused-vars": ["warn"],
+  },
 };
