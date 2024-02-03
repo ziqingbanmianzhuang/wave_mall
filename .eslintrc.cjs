662583fe304692b4e264ac25de50ce46176a9403
ziqing <1721370034@qq.com>
2024-02-03 11:58:30 +0800

    在.eslintrc.cjs配置@vue/eslint-config-typescript @rushstack/eslint-patch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,18 @@
 /* eslint-env node */
 // require('@rushstack/eslint-patch/modern-module-resolution')
-
+require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
 	root: true,
-	extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/eslint-config-typescript",
+	],
 	parserOptions: {
 		ecmaVersion: "latest",
 	},
 	rules: {
 		"vue/multi-word-component-names": ["warn"],
+		"@typescript-eslint/no-unused-vars": ["warn"],
 	},
 };
