c0c548761b1a34328ba58813109bfb4e4aa61d2d
ziqing <1721370034@qq.com>
2024-02-03 18:19:05 +0800

    fix(vite.config.js,index.vue): 在vite.config.js中配置tailwindcss,在Index.vue中使用tailwindcss
    后续tailwind可能还需要兼容小程序端
    
    BREAKING CHANGE: 在vite.config.ts中配置tailwindcss

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
-      <text class="title">
+      <text class="text-3xl font-bold underline">
         {{ title }}
       </text>
     </view>
@@ -15,29 +15,7 @@ const title = ref("Hello");
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import VueDevTools from "vite-plugin-vue-devtools";
 import uni from "@dcloudio/vite-plugin-uni";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni(), VueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
