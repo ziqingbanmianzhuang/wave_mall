cd3967fa0c1d8bc43136e50b4e0ebf22e28572d8
ziqing <1721370034@qq.com>
2024-02-03 11:05:12 +0800

    配置eslint.cjs,配置 'eslint:recommended'以及 ecmaVersion: 'latest'

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  
+}
