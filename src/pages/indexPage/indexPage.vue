e8432d63c6214d2ab493d9d5f8685bb54a2a0793
ziqing <1721370034@qq.com>
2024-02-04 23:20:41 +0800

    feat(indepage.vue): 获取分类组件的数据并完成渲染
    ...

--- src/pages/indexPage/indexPage.vue
+++ src/pages/indexPage/indexPage.vue
@@ -2,8 +2,8 @@
   <view></view>
   <view class="flex flex-wrap">
     <navigator
-      v-for="btn in btns"
-      :key="btn.id"
+      v-for="item in list"
+      :key="item.id"
       class="flex justify-start items-center box-border m-1.5 p-3 w-[175.5px] h-20 font-sans font-semibold text-4xs text-black bg-cyan-200 rounded-xl"
     >
       <view
@@ -11,27 +11,23 @@
       >
         <uni-icons type="contact" size="30" color="#000"></uni-icons>
       </view>
-      <text>{{ btn.name }}</text>
+      <text>{{ item.name }}</text>
     </navigator>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import type { CategoryItem } from "./indexPageType";
+import { getHomeCategoryAPI } from "./indexPageApi";
+import { onMounted } from "vue";
 
 //首页分类数据
-const btns = ref([
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-  { id: 1, name: "分类" },
-]);
+let list = ref<CategoryItem[]>([]);
+onMounted(async () => {
+  let res = await getHomeCategoryAPI();
+  list.value = res.result;
+});
 </script>
 
 <style>
