acf3065b2aa53929d3120a77266dcfc7b763d3db
ziqing <1721370034@qq.com>
2024-02-06 14:58:56 +0800

    feat(recommenditem.vue): 轮播图数据获取和渲染
    鼠标拖动时相邻轮播页会粘在一起,待解决

--- src/pages/recommendItem/recommendItem.vue
+++ src/pages/recommendItem/recommendItem.vue
@@ -1,7 +1,13 @@
 <template>
-  <uni-swiper-dot mode="round" info="" color="#ccc" current="" dots-styles="">
-    <swiper circular class="m-1.5 mb-6 h-28">
-      <swiper-item v-for="item in 5" :key="item">
+  <uni-swiper-dot
+    mode="round"
+    :info="swiperList"
+    color="#ccc"
+    :current="current"
+    :dots-styles="dotsStyles"
+  >
+    <swiper circular class="m-1.5 mb-6 h-28" @change="change">
+      <swiper-item v-for="item in swiperList" :key="item">
         <navigator
           url="/pages/hotItem/hotItem"
           open-type="navigate"
@@ -9,13 +15,17 @@
         >
           <view class="relative h-28">
             <image
-              src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-05-20/84abb5b1-8344-49ae-afc1-9cb932f3d593.jpg"
+              :src="item.imgUrl"
               mode="scaleToFill"
               class="w-[363px] h-28 rounded-xl"
             />
-            <text class="absolute left-2 bottom-2 text-white">the day you</text>
-            <view class="absolute right-2 top-2 bg-white rounded-xl">
-              <uni-icons type="contact" color="#000" size="24" />
+            <text class="absolute left-2 bottom-2 text-white">{{
+              item.type
+            }}</text>
+            <view
+              class="flex justify-center items-center w-6 h-6 absolute right-2 top-2 bg-white rounded-xl"
+            >
+              <uni-icons type="heart" color="#000" size="20" />
             </view>
           </view>
         </navigator>
@@ -24,7 +34,44 @@
   </uni-swiper-dot>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import type { SwiperItem } from "../indexPage/indexPageType";
+import { getHomeSwiperAPI } from "../indexPage/indexPageApi";
+import { onMounted } from "vue";
+
+//轮播图当前序号
+let current = ref(0);
+
+//轮播图数据
+
+const swiperList = ref<SwiperItem[]>([]);
+
+//轮播图点样式对象
+let dotsStyles = {
+  width: 8,
+  bottom: 5,
+  backgroundColor: "",
+  border: "0px solid skyblue",
+  selectedBackgroundColor: "",
+  selectedBorder: "",
+};
+
+//轮播图事件切换回调
+const change: UniHelper.SwiperOnChange = (e) => {
+  current.value = e.detail.current;
+};
+
+//获取轮播图数据
+const getHomeSwiperData = async () => {
+  let res = await getHomeSwiperAPI();
+  swiperList.value = res.result;
+  console.log("首页", swiperList.value);
+};
+onMounted(() => {
+  getHomeSwiperData();
+});
+</script>
 
 <style lang="scss" scoped>
 @import "tailwindcss/base";
