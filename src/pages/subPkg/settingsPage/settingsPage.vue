2d94508a3d3e5bc655cb513daa95d80cddba5352
ziqing <1721370034@qq.com>
2024-02-08 23:57:02 +0800

    feat(settingspage.vue): 设置页的静态页面
    ...

--- src/pages/subPkg/settingsPage/settingsPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="settingsPage">settingsPage</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped></style>
---
+++ src/subPkg/settingsPage/settingsPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <view
+    class="flex items-center border-b border-slate-300 border-solid w-[363px] m-1.5 mb-7 font-semibold"
+  >
+    <view
+      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-3 text-center leading-[32px]"
+    >
+      <uni-icons type="color" color="#fff" size="24" />
+    </view>
+    <text class="ml-1.5 mb-3">我的收货地址</text>
+  </view>
+  <view
+    class="flex items-center border-b border-slate-300 border-solid w-[363px] m-1.5 mb-7 font-semibold"
+  >
+    <view
+      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-3 text-center leading-[32px]"
+    >
+      <uni-icons type="color" color="#fff" size="24" />
+    </view>
+    <text class="ml-1.5 mb-3">授权管理</text>
+  </view>
+  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+    <view
+      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+    >
+      <uni-icons type="color" color="#fff" size="24" />
+    </view>
+    <text class="ml-1.5 mb-1.5">问题反馈</text>
+  </view>
+  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+    <view
+      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+    >
+      <uni-icons type="color" color="#fff" size="24" />
+    </view>
+    <text class="ml-1.5 mb-1.5">联系我们</text>
+  </view>
+  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+    <view
+      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+    >
+      <uni-icons type="color" color="#fff" size="24" />
+    </view>
+    <text class="ml-1.5 mb-1.5">关于本产品</text>
+  </view>
+  <button
+    class="bg-green-100 rounded-xl m-1.5 mt-9 w-[363px] h-12 leading-[48px] font-semibold"
+  >
+    退出登录
+  </button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
