dad89f5bf689294abdb8cc2be7332e1595ca2b74
ziqing <1721370034@qq.com>
2024-02-11 17:17:27 +0800

    feat(package.json,main.ts,store/index.ts,store/profile/index.ts): 安装pinia与Pinia持久化插件,配置pinia与持久化
    pinia需要下载2.0.36版本,否则vue3的版本会与最新的2.1.x版本冲突,考虑到升级vue3版本不可行
    
    BREAKING CHANGE: pinia需要下载2.0.36版本,否则vue3的版本会与最新的2.1.x版本冲突,考虑到升级vue3版本不可行

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
+import pinia from "./store/index";
+
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(pinia);
   return {
     app,
   };
