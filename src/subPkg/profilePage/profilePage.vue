e59e40dae2bf2b60635759dfa85c67e73e40081f
ziqing <1721370034@qq.com>
2024-02-11 22:08:00 +0800

    feat(profilepage.vue): 更新个人信息的表单
    收集表单填写的信息,向后端提交更新,并返回到上一页
    
    BREAKING CHANGE: 收集表单填写的信息,向后端提交更新,并返回到上一页

--- src/subPkg/profilePage/profilePage.vue
+++ src/subPkg/profilePage/profilePage.vue
@@ -23,15 +23,15 @@
       <view class="flex my-3">
         <text class="mr-20">昵称</text>
         <input
+          v-model="profile.nickname"
           type="text"
           placeholder="请填写昵称"
-          :value="profile?.nickname"
           class="text-gray-500"
         />
       </view>
       <view class="flex my-3">
         <text class="mr-20">性别</text>
-        <radio-group>
+        <radio-group @change="onGenderChange">
           <label>
             <radio
               value="男"
@@ -57,6 +57,7 @@
           :value="profile?.birthday"
           start="1900-01-01"
           :end="new Date()"
+          @change="onBirthdayChange"
         >
           <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
           <view v-else class="text-gray-500">请选择日期</view>
@@ -68,6 +69,7 @@
           class="picker"
           :value="profile?.fullLocation?.split(' ')"
           mode="region"
+          @change="onFullLocationChange"
         >
           <view v-if="profile?.fullLocation">{{ profile.fullLocation }}</view>
           <view v-else class="text-gray-500">请选择城市</view>
@@ -76,15 +78,16 @@
       <view class="flex my-3">
         <text class="mr-20">职业</text>
         <input
+          v-model="profile.profession"
           type="text"
           placeholder="请填写职业"
-          :value="profile?.profession"
           class="text-gray-500"
         />
       </view>
       <view class="flex my-3 justify-center">
         <button
           class="bg-slate-200 rounded-xl w-24 h-8 leading-[32px] text-white font-semibold"
+          @tap="onSubmit"
         >
           保存
         </button>
@@ -94,19 +97,36 @@
 </template>
 
 <script lang="ts" setup>
-import { getMemberProfileAPI } from "./profileApi";
-import type { ProfileDetail } from "./profileType";
+import { getMemberProfileAPI, putMemberProfileAPI } from "./profileApi";
+import type { ProfileDetail, Gender, ProfileParams } from "./profileType";
 import { ref, onMounted } from "vue";
 import { useProfileStore } from "../../store/profile/index";
 
 // 获取个人信息
-const profile = ref<ProfileDetail>();
+const profile = ref({} as ProfileDetail);
 const getMemberProfileData = async () => {
   const res = await getMemberProfileAPI();
   profile.value = res.result;
 };
 
-// 更新图像
+//修改性别
+const onGenderChange: UniHelper.RadioGroupOnChange = (ev) => {
+  profile.value.gender = ev.detail.value as Gender;
+};
+
+// 修改生日
+const onBirthdayChange: UniHelper.DatePickerOnChange = (ev) => {
+  profile.value.birthday = ev.detail.value;
+};
+
+// 修改城市
+let fullLocationCode: [string, string, string] = ["", "", ""];
+const onFullLocationChange: UniHelper.RegionPickerOnChange = (ev) => {
+  // 修改前端界面
+  profile.value.fullLocation = ev.detail.value.join(" ");
+  // 提交后端更新
+  fullLocationCode = ev.detail.code!;
+};
 
 const profileStore = useProfileStore();
 
@@ -145,6 +165,26 @@ const onAvatarChange = () => {
       });
     },
   });
+};
+
+// 点击保存提交表单
+const onSubmit = async () => {
+  const { nickname, gender, birthday, profession } = profile.value;
+  const res = await putMemberProfileAPI({
+    nickname,
+    gender,
+    birthday,
+    profession,
+    provinceCode: fullLocationCode[0],
+    cityCode: fullLocationCode[1],
+    countyCode: fullLocationCode[2],
+  });
+  // 更新Store昵称
+  profileStore.profile!.nickname = res.result.nickname;
+  uni.showToast({ icon: "success", title: "保存成功" });
+  setTimeout(() => {
+    uni.navigateBack();
+  }, 400);
 };
 onMounted(() => {
   getMemberProfileData();
--- src/subPkg/profilePage/profileType.d.ts
+++ src/subPkg/profilePage/profileType.d.ts
@@ -19,3 +19,16 @@ export type ProfileDetail = {
 };
 /** 性别 */
 export type Gender = "女" | "男";
+
+/** 个人信息 修改请求体参数 */
+export type ProfileParams = Pick<
+  ProfileDetail,
+  "nickname" | "gender" | "birthday" | "profession"
+> & {
+  /** 省份编码 */
+  provinceCode?: string;
+  /** 城市编码 */
+  cityCode?: string;
+  /** 区/县编码 */
+  countyCode?: string;
+};
