2db2bade850c71c34ab419aebb9088fb9b8ae4de
ziqing <1721370034@qq.com>
2024-02-16 23:01:40 +0800

    feat(detailpage.vue): 完成 订单详情页 的静态布局

--- src/orderPkg/detailOrderPage/detailOrderPage.vue
+++ src/orderPkg/detailOrderPage/detailOrderPage.vue
@@ -1,7 +1,99 @@
 <template>
-  <div class="detailOrderPage">detailOrderPage</div>
+  <!-- 买家信息 -->
+  <view class="shadow-lg rounded-xl mx-1.5 px-3 w-[363px] h-32">
+    <view
+      class="flex items-center justify-start border-b border-slate-300 border-solid w-full"
+    >
+      <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text>
+      <view class="flex flex-col my-3">
+        <text class="font-semibold">物流信息</text>
+        <text class="text-xs text-slate-300">2023 04 19</text>
+      </view>
+    </view>
+    <view class="flex items-center justify-start my-3 w-full">
+      <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text>
+      <view
+        ><view class="font-semibold">
+          <text class="mr-3">姓名</text>
+          <text>+12345678</text>
+        </view>
+        <text class="text-xs text-slate-300">广东 广州</text>
+      </view></view
+    >
+  </view>
+
+  <!-- 订单商品 -->
+  <view>
+    <view
+      v-for="item in 2"
+      :key="item"
+      class="flex justify-start shadow-lg rounded-xl mx-1.5 px-3 w-[363px] h-32"
+    >
+      <image src="" mode="scaleToFill" class="rounded-xl mr-3 w-16 h-16" />
+      <view class="flex flex-col justify-around">
+        <text class="font-semibold">商品名</text>
+        <text class="text-slate-300 text-xs">11 22 33</text>
+        <text class="text-orange-300">200</text>
+      </view>
+    </view>
+  </view>
+
+  <!-- 评价,售后 -->
+  <view class="flex justify-center mt-3 w-full">
+    <button
+      class="rounded-xl border border-slate-300 border-solid mr-3 w-16 h-8 leading-8"
+    >
+      去评价
+    </button>
+    <button
+      class="rounded-xl border border-slate-300 border-solid w-16 h-8 font-semibold leading-8"
+    >
+      去售后
+    </button>
+  </view>
+
+  <!-- 账目 -->
+  <view class="shadow-lg rounded-xl mx-1.5 w-[363px] h-32 font-semibold">
+    <view class="flex justify-between items-center">
+      <view class="flex items-center">
+        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"> </text>
+        <text>商品总价</text></view
+      >
+      <text class="text-slate-300 text-xs">$69</text>
+    </view>
+    <view class="flex justify-between items-center">
+      <view class="flex items-center">
+        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text
+        ><text>商品运费</text></view
+      >
+      <text class="text-slate-300 text-xs">$56</text>
+    </view>
+    <view class="flex justify-between items-center">
+      <view class="flex items-center">
+        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text
+        ><text>应付金额</text></view
+      >
+      <text class="text-orange-300">$45</text>
+    </view>
+  </view>
+
+  <!-- 支付剩余 -->
+  <view class="text-center font-semibold">
+    <text class="mr-3">应付金额:$12</text>
+    <text>支付剩余:00小时</text>
+  </view>
+
+  <!-- 取消 or 支付订单 -->
+  <view class="flex justify-between mx-1.5 text-white">
+    <button class="bg-green-300 rounded-xl w-28 h-8 leading-8">取消订单</button>
+    <button class="bg-red-300 rounded-xl w-36 h-8 leading-8">去支付</button>
+  </view>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
