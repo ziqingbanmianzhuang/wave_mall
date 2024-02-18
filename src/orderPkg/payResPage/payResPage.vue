d07a59d440eef135e8557358069282ab4d392fff
ziqing <1721370034@qq.com>
2024-02-18 16:43:18 +0800

    feat(pages.json,payrespage.vue): 订单支付跳转支付结果页
    支付成功之后订单状态为已支付,跳转支付结果页

---
+++ src/orderPkg/payResPage/payResPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>支付成功</view>
+  <navigator
+    hover-class="none"
+    class="button navigator"
+    url="/pages/indexPage/indexPage"
+    open-type="switchTab"
+  >
+    返回首页
+  </navigator>
+  <navigator
+    hover-class="none"
+    class="button navigator"
+    :url="`/orderPkg/detailOrderPage/detailOrderPage?id=${query.id}`"
+    open-type="redirect"
+  >
+    查看订单
+  </navigator>
+</template>
+
+<script lang="ts" setup>
+// 获取页面参数
+const query = defineProps<{
+  id: string;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
