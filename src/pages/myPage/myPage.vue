f8b9477b7762b45c70a50ed601d6e9514cdbcf01
ziqing <1721370034@qq.com>
2024-02-24 17:22:03 +0800

    fix(mypage.vue): 获取渲染用户信息，统一页面font,color等，修改页面布局和样式

--- src/pages/myPage/myPage.vue
+++ src/pages/myPage/myPage.vue
@@ -1,89 +1,125 @@
 <template>
-  <view class="bg-slate-100 rounded-xl mx-1.5 mt-1.5 mb-9 w-[363px] h-36">
+  <view class="bg-primary w-full h-full">
     <!-- 用户信息 -->
-    <view class="flex relative p-3">
-      <view class="bg-white rounded-xl w-14 h-14"> </view>
-      <view class="flex flex-col font-semibold ml-3">
-        <navigator class="mb-3 text-base" url="/subPkg/profilePage/profilePage"
-          >昵称</navigator
-        >
-        <text class="text-xs text-gray-400">更新用户昵称</text>
-      </view>
-      <navigator
-        class="absolute right-1.5 top-1.5"
-        url="/subPkg/settingsPage/settingsPage"
-      >
-        <uni-icons type="gear-filled" color="" size="24" />
-      </navigator>
-    </view>
-    <!-- 物流 -->
-    <view class="flex justify-between p-3 text-sm font-semibold">
-      <view class="flex flex-col">
-        <text>待付款</text>
-        <view
-          class="bg-white h-6 w-6 rounded-[12px] text-center leading-[24px]"
-        >
-          <uni-icons type="color" color="" size="18" />
-        </view>
-      </view>
-
-      <view class="flex flex-col">
-        <text>待发货</text>
-        <view
-          class="bg-white h-6 w-6 rounded-[12px] text-center leading-[24px]"
-        >
-          <uni-icons type="color" color="" size="18" />
-        </view>
-      </view>
-
-      <view class="flex flex-col">
-        <text>待收货</text>
-        <view
-          class="bg-white h-6 w-6 rounded-[12px] text-center leading-[24px]"
-        >
-          <uni-icons type="color" color="" size="18" />
-        </view>
-      </view>
-
-      <view class="flex flex-col">
-        <text>待评价</text>
-        <view
-          class="bg-white h-6 w-6 rounded-[12px] text-center leading-[24px]"
-        >
-          <uni-icons type="color" color="" size="18" />
-        </view>
-      </view>
-
-      <view class="flex flex-col">
-        <text>售后</text>
-        <view
-          class="bg-white h-6 w-6 rounded-[12px] text-center leading-[24px]"
-        >
-          <uni-icons type="color" color="" size="18" />
-        </view>
-      </view>
-    </view>
-  </view>
-  <!-- 订单 -->
-  <view
-    class="flex justify-between bg-white shadow-lg rounded-xl m-1.5 p-3 h-16 leading-[64px] w-[363px] text-base font-semibold"
-  >
-    <text>我的订单</text>
-    <navigator url="/pages/orderListPage/orderListPage?type='0'"
-      >查看全部订单</navigator
+    <view
+      class="bg-secondary border-radius-primary margin-x-primary mt-1.5 mb-9 w-[363px] h-36"
     >
+      <!-- 用户信息 -->
+      <view class="flex relative p-3">
+        <image
+          class="bg-primary border-radius-primary w-14 h-14"
+          :src="profile?.avatar"
+        >
+        </image>
+        <view class="flex flex-col justify-around ml-3">
+          <view class="flex">
+            <text class="mr-1.5 font-primary"> {{ profile?.nickname }}</text>
+            <navigator url="/subPkg/profilePage/profilePage">
+              <uni-icons type="compose" color="#b4b5c5" size="20" />
+            </navigator>
+          </view>
+          <view class="flex">
+            <text class="font-secondary mr-1.5">
+              {{ profile?.gender ? profile?.gender : "性别未知" }}</text
+            >
+            <text class="font-secondary">{{
+              profile?.fullLocation ? profile?.fullLocation : "地址未知"
+            }}</text>
+          </view>
+        </view>
+        <navigator
+          class="absolute right-1.5 top-1.5"
+          url="/subPkg/settingsPage/settingsPage"
+        >
+          <uni-icons type="gear-filled" color="#c2410c" size="20" />
+        </navigator>
+      </view>
+      <!-- 物流 -->
+      <view class="flex justify-between p-3">
+        <navigator
+          v-for="dis in distributions"
+          :key="dis.title"
+          class="flex flex-col justify-around items-center"
+          url="/pages/orderListPage/orderListPage"
+        >
+          <view
+            class="bg-white h-6 w-6 border-radius-primary text-center leading-[24px]"
+          >
+            <uni-icons :type="dis.icon" :color="dis.color" size="20" />
+          </view>
+          <text class="font-primary-smaller">{{ dis.title }}</text>
+        </navigator>
+      </view>
+    </view>
+    <!-- 订单 -->
+    <view
+      class="flex justify-around items-center bg-secondary border-radius-primary margin-x-primary my-1.5 p-3 h-16 leading-[64px] w-[363px] font-primary-smaller"
+    >
+      <text>我的订单</text>
+      <navigator url="/pages/orderListPage/orderListPage?type='0'"
+        >查看全部订单</navigator
+      >
+    </view>
+    <!-- 登录 -->
+    <navigator
+      url="/pages/loginPage/loginPage"
+      open-type="navigate"
+      hover-class="navigator-hover"
+    >
+      登录
+    </navigator>
   </view>
-  <!-- 登录 -->
-  <navigator
-    url="/pages/loginPage/loginPage"
-    open-type="navigate"
-    hover-class="navigator-hover"
-  >
-    登录
-  </navigator>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { _UniIconsType, ProfileDetail } from "./myPageType";
+import { getMemberProfileAPI } from "./myPageApi";
+interface distributionsType {
+  title: string;
+  icon: _UniIconsType;
+  color: string;
+}
+const distributions = ref<distributionsType[]>([
+  {
+    title: "待付款",
+    icon: "weixin",
+    color: "#22c55e",
+  },
+  {
+    title: "待发货",
+    icon: "home-filled",
+    color: "#637FF1",
+  },
+  {
+    title: "待收货",
+    icon: "notification-filled",
+    color: "#fdba74",
+  },
+  {
+    title: "待评价",
+    icon: "notification-filled",
+    color: "#c2410c",
+  },
+  {
+    title: "售后",
+    icon: "hand-up-filled",
+    color: "#F5C60B",
+  },
+]);
+//用户信息
+const profile = ref<ProfileDetail>();
+
+// 获取用户信息
+const getMemberProfileData = async () => {
+  const res = await getMemberProfileAPI();
+  profile.value = res.result;
+};
+
+onMounted(() => {
+  getMemberProfileData();
+});
+</script>
 
 <style lang="scss" scoped>
 @import "tailwindcss/base";
