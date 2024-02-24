387105149850847917b37c983b44f4b093368bfc
ziqing <1721370034@qq.com>
2024-02-24 18:20:40 +0800

    fix(profilepage.vue): 统一 color,fontSize等
    input组件的聚集样式影响到placeholder样式，该问题待解决

--- src/subPkg/profilePage/profilePage.vue
+++ src/subPkg/profilePage/profilePage.vue
@@ -1,96 +1,113 @@
 <template>
-  <view class="bg-slate-200 rounded-xl w-[375px] h-24"></view>
-  <view class="relative top-[-30px] left-0 shadow-lg bg-white rounded-xl m-1.5">
-    <!-- 头像昵称 -->
-    <view class="flex flex-col justify-center items-center text-center">
+  <view class="bg-primary w-full h-full">
+    <view class="bg-slate-200 w-full h-24">
       <image
         :src="profile?.avatar"
         mode="aspectFill"
-        class="absolute top-[-40px] rounded-[24px] h-12 w-12"
+        class="w-full h-24"
         @tap="onAvatarChange"
       />
-      <text
-        class="block border-b border-gray-300 border-solid mt-3 w-full text-base font-semibold"
-        >{{ profile?.account }}</text
-      >
     </view>
-    <!-- 表单 -->
-    <view class="p-3">
-      <view class="flex my-3">
-        <text class="mr-20">账号</text>
-        <text class="text-gray-500">ziqing</text>
-      </view>
-      <view class="flex my-3">
-        <text class="mr-20">昵称</text>
-        <input
-          v-model="profile.nickname"
-          type="text"
-          placeholder="请填写昵称"
-          class="text-gray-500"
+
+    <view
+      class="relative top-[-30px] left-0 bg-white border-radius-primary m-1.5"
+    >
+      <!-- 头像昵称 -->
+      <view class="flex flex-col justify-center items-center text-center">
+        <image
+          :src="profile?.avatar"
+          mode="aspectFill"
+          class="absolute top-[-40px] border-radius-primary h-12 w-12"
+          @tap="onAvatarChange"
         />
-      </view>
-      <view class="flex my-3">
-        <text class="mr-20">性别</text>
-        <radio-group @change="onGenderChange">
-          <label>
-            <radio
-              value="男"
-              color="#27ba9b"
-              :checked="profile?.gender === '男'"
-            />
-            <text class="text-gray-500 mr-3">男</text>
-          </label>
-          <label>
-            <radio
-              value="女"
-              color="#27ba9b"
-              :checked="profile?.gender === '女'"
-            />
-            <text class="text-gray-500">女</text>
-          </label>
-        </radio-group>
-      </view>
-      <view class="flex my-3">
-        <text class="mr-20">生日</text>
-        <picker
-          mode="date"
-          :value="profile?.birthday"
-          start="1900-01-01"
-          :end="new Date()"
-          @change="onBirthdayChange"
+        <text
+          class="block border-b border-gray-300 border-solid mt-3 w-full font-primary"
+          >{{ profile?.account }}</text
         >
-          <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
-          <view v-else class="text-gray-500">请选择日期</view>
-        </picker>
       </view>
-      <view class="flex my-3">
-        <text class="mr-20">城市</text>
-        <picker
-          class="picker"
-          :value="profile?.fullLocation?.split(' ')"
-          mode="region"
-          @change="onFullLocationChange"
-        >
-          <view v-if="profile?.fullLocation">{{ profile.fullLocation }}</view>
-          <view v-else class="text-gray-500">请选择城市</view>
-        </picker>
-      </view>
-      <view class="flex my-3">
-        <text class="mr-20">职业</text>
-        <input
-          v-model="profile.profession"
-          type="text"
-          placeholder="请填写职业"
-          class="text-gray-500"
-        />
-      </view>
-      <view class="flex my-3 justify-center">
-        <button
-          class="bg-slate-200 rounded-xl w-24 h-8 leading-[32px] text-white font-semibold"
-          @tap="onSubmit"
-        >
-          保存
-        </button>
+      <!-- 表单 -->
+      <view class="p-3">
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">账号</text>
+          <text class="font-secondary">ziqing</text>
+        </view>
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">昵称</text>
+          <input
+            v-model="profile.nickname"
+            type="text"
+            placeholder="请填写昵称"
+            placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
+            class="input-primary"
+          />
+        </view>
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">性别</text>
+          <radio-group @change="onGenderChange">
+            <label>
+              <radio
+                value="男"
+                color="#27ba9b"
+                :checked="profile?.gender === '男'"
+                style="transform: scale(0.7)"
+              />
+              <text class="font-secondary mr-3">男</text>
+            </label>
+            <label>
+              <radio
+                value="女"
+                color="#27ba9b"
+                :checked="profile?.gender === '女'"
+                style="transform: scale(0.7)"
+              />
+              <text class="font-secondary">女</text>
+            </label>
+          </radio-group>
+        </view>
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">生日</text>
+          <picker
+            mode="date"
+            :value="profile?.birthday"
+            start="1900-01-01"
+            :end="new Date()"
+            class="input-primary"
+            @change="onBirthdayChange"
+          >
+            <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
+            <view v-else class="font-secondary">请选择日期</view>
+          </picker>
+        </view>
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">城市</text>
+          <picker
+            class="picker input-primary"
+            :value="profile?.fullLocation?.split(' ')"
+            mode="region"
+            @change="onFullLocationChange"
+          >
+            <view v-if="profile?.fullLocation">{{ profile.fullLocation }}</view>
+            <view v-else class="font-secondary">请选择城市</view>
+          </picker>
+        </view>
+        <view class="flex my-3">
+          <text class="mr-20 font-primary-smaller">职业</text>
+          <input
+            v-model="profile.profession"
+            type="text"
+            placeholder="请填写职业"
+            placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
+            class="input-primary"
+          />
+        </view>
+        <view class="flex mt-12 justify-center">
+          <button
+            class="bg-orange-500 border-radius-primary w-24 h-8 leading-[32px] text-white font-primary-smaller"
+            @tap="onSubmit"
+          >
+            保存
+          </button>
+        </view>
       </view>
     </view>
   </view>
@@ -195,4 +212,11 @@ onMounted(() => {
 @import "tailwindcss/base";
 @import "tailwindcss/components";
 @import "tailwindcss/utilities";
+.input-primary {
+  font-size: 14px;
+  font-weight: 600;
+  font-family: "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  color: #2b2d42;
+}
 </style>
