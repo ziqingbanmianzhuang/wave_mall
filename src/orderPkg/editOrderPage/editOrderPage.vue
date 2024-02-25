bea3ff06f5856ed38259159a39d6a92df2c897a4
ziqing <1721370034@qq.com>
2024-02-25 20:14:29 +0800

    style(editordrpage.vue): 统一 订单表单页的前端界面设计

--- src/orderPkg/editOrderPage/editOrderPage.vue
+++ src/orderPkg/editOrderPage/editOrderPage.vue
@@ -1,100 +1,120 @@
 <template>
-  <!-- 选择收货地址 -->
-  <view
-    class="box-border flex bg-blue-100 rounded-xl m-1.5 mb-3 px-1.5 w-[363px] h-32 leading-[32px] font-semibold"
-  >
-    <navigator
-      url="/subPkg/addressPage/addressPage"
-      open-type="navigate"
-      hover-class="navigator-hover"
+  <view class="bg-primary w-full">
+    <!-- 选择收货地址 -->
+    <view
+      class="box-border flex items-center bg-sky-100 border-radius-primary margin-x-primary my-1.5 mb-3 px-3 w-[363px] h-12 leading-[32px]"
     >
-      <uni-icons type="plusempty" class="mr-1.5"></uni-icons>
-      <text>请选择收货地址</text>
-      <view class="user">
-        {{ selecteAddress?.receiver }} {{ selecteAddress?.contact }}
-      </view>
-      <view class="address">
-        {{ selecteAddress?.fullLocation }} {{ selecteAddress?.address }}
-      </view>
-    </navigator>
-  </view>
-
-  <!-- 商品信息 -->
-  <view
-    v-for="good in orderData?.goods"
-    :key="good.id"
-    class="flex items-center shadow-lg rounded-xl mx-1.5 mb-3 px-1.5 w-[363px] h-32"
-  >
-    <view class="bg-red-300 rounded-[8px] h-2 w-2"></view>
-    <image
-      class="rounded-xl w-16 h-16"
-      :src="good.picture"
-      mode="scaleToFill"
-    />
-    <view class="flex flex-col flex-1">
-      <text class="font-semibold">{{ good.name }}</text>
-      <text class="text-xs text-slate-300">{{ good.attrsText }}</text>
-      <view class="flex justify-between"
-        ><text class="text-red-300 font-semibold">{{ good.totalPrice }}</text
-        ><text class="font-semibold"> X {{ good.count }}</text></view
+      <navigator
+        url="/subPkg/addressPage/addressPage"
+        open-type="navigate"
+        hover-class="navigator-hover"
       >
-    </view>
-  </view>
-
-  <!-- 商品订单信息 -->
-  <view class="shadow-lg rounded-xl mx-1.5 mb-3 px-1.5 w-[363px] h-40">
-    <view
-      class="flex justify-between items-center border-b border-slate-300 border-solid pb-3"
-    >
-      <view class="flex items-center">
-        <view class="bg-orange-300 mr-3 rounded-[8px] h-2 w-2"></view>
-        <view class="flex flex-col font-semibold">
-          <text>商品总价</text>
-          <text>商品运费</text>
+        <view v-if="selecteAddress" class="flex items-center">
+          <uni-icons type="home" color="#fdba74" size="20" />
+          <view class="ml-1.5">
+            <view class="font-primary-smaller">
+              {{ selecteAddress?.receiver }} +{{ selecteAddress?.contact }}
+            </view>
+            <view class="font-secondary">
+              {{ selecteAddress?.fullLocation }}
+              {{ selecteAddress?.address }}
+            </view>
+          </view>
         </view>
-      </view>
-      <view class="flex flex-col text-xs font-semibold text-red-300">
-        <text>${{ orderData?.summary.totalPrice }}</text>
-        <text>${{ orderData?.summary.postFee }}</text>
-      </view>
+        <template v-else>
+          <uni-icons type="plusempty" class="mr-1.5" size="20"></uni-icons>
+          <text class="font-primary">请选择收货地址</text>
+        </template>
+      </navigator>
     </view>
-    <view
-      class="flex justify-between items-center border-b border-slate-300 border-solid pb-3"
-    >
-      <view class="flex items-center">
-        <view class="bg-orange-300 mr-3 rounded-[8px] h-2 w-2"></view>
-        <view class="flex flex-col font-semibold">
-          <text>配送时间</text>
 
-          <text>订单备注</text>
-        </view>
-      </view>
-      <view class="flex flex-col text-xs font-semibold text-slate-300">
-        <uni-data-select
-          v-model="activeIndex"
-          :localdata="deliveryList"
-          :label="activeDelivery"
-          @change="changeDelivery"
+    <!-- 商品信息 -->
+    <view
+      v-for="good in orderData?.goods"
+      :key="good.id"
+      class="flex justify-between items-center bg-secondary border-radius-primary margin-x-primary mb-3 px-1.5 w-[363px] h-32"
+    >
+      <view class="bg-orange-500 rounded-[8px] mr-3 h-2 w-2"></view>
+      <image
+        class="border-radius-primary mr-3 w-16 h-16"
+        :src="good.picture"
+        mode="aspectFill"
+      />
+      <view class="flex flex-col flex-1">
+        <text class="font-primary-smaller">{{ good.name }}</text>
+        <text class="font-secondary">{{ good.attrsText }}</text>
+        <view class="flex justify-between"
+          ><text class="font-secondary font-yellow">{{ good.totalPrice }}</text
+          ><text class="font-secondary"> X {{ good.count }}</text></view
         >
-        </uni-data-select>
-        <text>建议留言</text>
       </view>
     </view>
-  </view>
-  <!--提交订单 -->
-  <view
-    class="flex justify-between items-center bg-blue-300 rounded-xl mx-1.5 px-1.5 w-[363px] h-32 font-semibold text-xs"
-  >
-    <view class="flex flex-col">
-      <text class="text-red-300">${{ orderData?.summary.totalPayPrice }}</text>
-      <text class="text-slate-300">黄色小象</text>
-    </view>
-    <button
-      class="bg-green-400 rounded-xl text-white w-16 h-8 leading-8"
-      @tap="orderSubmit"
+
+    <!-- 商品订单信息 -->
+    <view
+      class="bg-secondary border-radius-primary margin-x-primary mb-3 px-1.5 w-[363px] h-40"
     >
-      提交订单
-    </button>
+      <view
+        class="flex justify-between items-center border-b border-slate-300 border-solid pb-3"
+      >
+        <view class="flex items-center">
+          <view class="bg-orange-500 mr-3 rounded-[8px] h-2 w-2"></view>
+          <view class="flex flex-col">
+            <text class="font-primary-smaller">商品总价</text>
+            <text class="font-primary-smaller">商品运费</text>
+          </view>
+        </view>
+        <view class="flex flex-col">
+          <text class="font-secondary font-yellow"
+            >${{ orderData?.summary.totalPrice }}</text
+          >
+          <text class="font-secondary font-yellow"
+            >${{ orderData?.summary.postFee }}</text
+          >
+        </view>
+      </view>
+      <view
+        class="flex justify-between items-center border-b border-slate-300 border-solid pb-3"
+      >
+        <view class="flex items-center">
+          <view class="bg-orange-500 mr-3 rounded-[8px] h-2 w-2"></view>
+          <view class="flex flex-col">
+            <text class="font-primary-smaller">配送时间</text>
+            <text class="font-primary-smaller">订单备注</text>
+          </view>
+        </view>
+        <view class="flex flex-col items-end">
+          <uni-data-select
+            v-model="activeIndex"
+            style="width: 170px"
+            placeholder="时间不限"
+            :localdata="deliveryList"
+            :label="activeDelivery"
+            class="w-20 font-secondary"
+            @change="changeDelivery"
+          >
+          </uni-data-select>
+          <text class="font-secondary">建议留言</text>
+        </view>
+      </view>
+    </view>
+    <!--提交订单 -->
+    <view
+      class="flex justify-between items-center bg-sky-100 border-radius-primary margin-x-primary px-1.5 w-[363px] h-16"
+    >
+      <view class="flex flex-col justify-around h-full">
+        <text class="font-primary font-yellow"
+          >${{ orderData?.summary.totalPayPrice }}</text
+        >
+        <text class="font-secondary">黄色小象</text>
+      </view>
+      <button
+        class="bg-[#266972] border-radius-primary text-white w-16 h-8 leading-8 font-primary-smaller"
+        @tap="orderSubmit"
+      >
+        提交订单
+      </button>
+    </view>
   </view>
 </template>
 
