52d7196f19fd4866f057272b899b7d6fba5dbeb9
ziqing <1721370034@qq.com>
2024-02-25 14:58:31 +0800

    style(settingspage.vue): 统一 设置页面的font,color,border等,重新设计图标
    后续还需要重新设计一个系统的图标方案

--- src/subPkg/settingsPage/settingsPage.vue
+++ src/subPkg/settingsPage/settingsPage.vue
@@ -1,52 +1,59 @@
 <template>
-  <view
-    class="flex items-center border-b border-slate-300 border-solid w-[363px] m-1.5 mb-7 font-semibold"
-  >
+  <!-- 我的收货地址 -->
+  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
     <view
-      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-3 text-center leading-[32px]"
+      class="border-b border-[#22c55e] border-solid border-radius-primary mr-1.5 mb-3 w-6 h-6 leading-[24px] text-center"
     >
-      <uni-icons type="color" color="#fff" size="24" />
+      <uni-icons type="home" color="#22c55e" size="20" />
     </view>
-    <navigator class="ml-1.5 mb-3" url="/subPkg/addressPage/addressPage"
+    <navigator
+      class="ml-1.5 mb-3 font-primary-smaller"
+      url="/subPkg/addressPage/addressPage"
       >我的收货地址</navigator
     >
   </view>
+  <!-- 授权管理 -->
   <view
-    class="flex items-center border-b border-slate-300 border-solid w-[363px] m-1.5 mb-7 font-semibold"
+    class="flex items-center border-b border-slate-300 border-solid w-[363px] margin-x-primary my-1.5 mb-7"
   >
     <view
-      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-3 text-center leading-[32px]"
+      class="w-6 h-6 border-radius-primary mr-1.5 mb-3 text-center leading-[24px] border-b border-[#c2410c] border-solid w"
     >
-      <uni-icons type="color" color="#fff" size="24" />
+      <uni-icons type="info" color="#c2410c" size="20" />
     </view>
-    <text class="ml-1.5 mb-3">授权管理</text>
+    <text class="ml-1.5 mb-3 font-primary-smaller">授权管理</text>
   </view>
-  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+  <!-- 问题反馈 -->
+  <view
+    class="flex items-center w-[363px] margin-x-primary my-1.5 font-semibold"
+  >
     <view
-      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+      class="w-6 h-6 6 border-radius-primary border-b border-[#F5C60B] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
-      <uni-icons type="color" color="#fff" size="24" />
+      <uni-icons type="help" color="#F5C60B" size="20" />
     </view>
-    <text class="ml-1.5 mb-1.5">问题反馈</text>
+    <text class="ml-1.5 mb-1.5 font-primary-smaller">问题反馈</text>
   </view>
-  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+  <!-- 联系我们 -->
+  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
     <view
-      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+      class="w-6 h-6 border-radius-primary border-b border-[#637FF1] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
-      <uni-icons type="color" color="#fff" size="24" />
+      <uni-icons type="phone" color="#637FF1" size="24" />
     </view>
-    <text class="ml-1.5 mb-1.5">联系我们</text>
+    <text class="ml-1.5 mb-1.5 font-primary">联系我们</text>
   </view>
-  <view class="flex items-center w-[363px] m-1.5 font-semibold">
+  <!-- 关于本产品 -->
+  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
     <view
-      class="w-8 h-8 bg-green-200 rounded-xl mr-1.5 mb-1.5 text-center leading-[32px]"
+      class="w-6 h-6 border-radius-primary border-b border-[#fdba74] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
-      <uni-icons type="color" color="#fff" size="24" />
+      <uni-icons type="paperplane" color="#fdba74" size="24" />
     </view>
-    <text class="ml-1.5 mb-1.5">关于本产品</text>
+    <text class="ml-1.5 mb-1.5 font-primary-smaller">关于本产品</text>
   </view>
   <button
-    class="bg-green-100 rounded-xl m-1.5 mt-9 w-[363px] h-12 leading-[48px] font-semibold"
+    class="bg-orange-800 border-radius-primary m-1.5 mt-9 w-[363px] h-12 leading-[48px] font-base font-semibold text-white"
   >
     退出登录
   </button>
