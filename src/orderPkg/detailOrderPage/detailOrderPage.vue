058e7919f409938e850a082c36c1054faade5507
ziqing <1721370034@qq.com>
2024-02-25 21:01:47 +0800

    style(detailorderpage.vue): 标准化 订单界面的前端布局设计

--- src/orderPkg/detailOrderPage/detailOrderPage.vue
+++ src/orderPkg/detailOrderPage/detailOrderPage.vue
@@ -1,146 +1,149 @@
 <template>
-  <!-- 买家信息 -->
-  <view class="shadow-lg rounded-xl mx-1.5 px-3 w-[363px] h-32">
+  <view class="bg-primary">
+    <!-- 买家信息 -->
     <view
-      class="flex items-center justify-start border-b border-slate-300 border-solid w-full"
+      class="bg-secondary border-radius-primary margin-x-primary px-3 mb-3 w-[363px] h-32"
     >
-      <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text>
-      <view class="flex flex-col my-3">
-        <text class="font-semibold">下单时间</text>
-        <text class="text-xs text-slate-300">{{
-          orderDetail?.createTime
-        }}</text>
-      </view>
-    </view>
-    <view class="flex items-center justify-start my-3 w-full">
-      <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text>
       <view
-        ><view class="font-semibold">
-          <text class="mr-3">{{ orderDetail?.receiverContact }}</text>
-          <text>+{{ orderDetail?.receiverMobile }}</text>
+        class="flex items-center justify-start border-b border-slate-300 border-solid w-full"
+      >
+        <text class="block bg-orange-800 rounded-[8px] mr-3 w-2 h-2"></text>
+        <view class="flex flex-col my-3">
+          <text class="font-primary-smaller">下单时间</text>
+          <text class="font-secondary">{{ orderDetail?.createTime }}</text>
         </view>
-        <text class="text-xs text-slate-300">{{
-          orderDetail?.receiverAddress
-        }}</text>
-      </view></view
-    >
-  </view>
+      </view>
+      <view class="flex items-center justify-start my-3 w-full">
+        <text class="block bg-orange-800 rounded-[8px] mr-3 w-2 h-2"></text>
+        <view
+          ><view class="mr-3 font-primary-smaller">
+            <text>{{ orderDetail?.receiverContact }}</text>
+            <text>+{{ orderDetail?.receiverMobile }}</text>
+          </view>
+          <text class="font-secondary">{{ orderDetail?.receiverAddress }}</text>
+        </view></view
+      >
+    </view>
 
-  <!-- 订单商品 -->
-  <view>
-    <view
-      v-for="item in orderDetail?.skus"
-      :key="item.id"
-      class="flex justify-start shadow-lg rounded-xl mx-1.5 px-3 w-[363px] h-32"
-    >
-      <image
-        :src="item.image"
-        mode="scaleToFill"
-        class="rounded-xl mr-3 w-16 h-16"
-      />
-      <view class="flex flex-col justify-around">
-        <text class="font-semibold">{{ item.name }}</text>
-        <text class="text-slate-300 text-xs">{{ item.attrsText }}</text>
-        <text class="text-orange-300">X {{ item.quantity }} </text>
+    <!-- 订单商品 -->
+    <view>
+      <view
+        v-for="item in orderDetail?.skus"
+        :key="item.id"
+        class="relative flex justify-start items-center bg-secondary border-radius-primary margin-x-primary mb-3 px-3 w-[363px] h-20"
+      >
+        <image
+          :src="item.image"
+          mode="aspectFill"
+          class="border-radius-primary mr-3 w-16 h-16"
+        />
+        <view class="flex flex-col justify-around">
+          <text class="font-primary">{{ item.name }}</text>
+          <text class="font-secondary">{{ item.attrsText }}</text>
+          <text class="font-secondary font-yellow">X {{ item.quantity }} </text>
+        </view>
+        <three-dots></three-dots>
       </view>
     </view>
-  </view>
 
-  <!-- 评价,售后 -->
-  <view class="flex justify-center mt-3 w-full">
-    <button
-      class="rounded-xl border border-slate-300 border-solid mr-3 w-16 h-8 leading-8"
-    >
-      去评价
-    </button>
-    <button
-      class="rounded-xl border border-slate-300 border-solid w-16 h-8 font-semibold leading-8"
-    >
-      去售后
-    </button>
-  </view>
-
-  <!-- 账目 -->
-  <view class="shadow-lg rounded-xl mx-1.5 w-[363px] h-32 font-semibold">
-    <view class="flex justify-between items-center">
-      <view class="flex items-center">
-        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"> </text>
-        <text>商品总价</text></view
+    <!-- 评价,售后 -->
+    <view class="flex justify-center my-3 w-full">
+      <button
+        class="border-radius-primary border border-slate-300 border-solid mr-3 w-16 h-8 leading-8"
       >
-      <text class="text-slate-300 text-xs">{{ orderDetail?.totalMoney }}</text>
-    </view>
-    <view class="flex justify-between items-center">
-      <view class="flex items-center">
-        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text
-        ><text>商品运费</text></view
+        去评价
+      </button>
+      <button
+        class="border-radius-primary border border-slate-300 border-solid w-16 h-8 leading-8"
       >
-      <text class="text-slate-300 text-xs">{{ orderDetail?.postFee }}</text>
+        去售后
+      </button>
     </view>
-    <view class="flex justify-between items-center">
-      <view class="flex items-center">
-        <text class="block bg-orange-300 rounded-[8px] mr-3 w-2 h-2"></text
-        ><text>应付金额</text></view
-      >
-      <text class="text-orange-300">{{ orderDetail?.payMoney }}</text>
-    </view>
-  </view>
 
-  <!-- 支付剩余 -->
-  <view
-    v-if="orderDetail?.orderState === OrderState.DaiFuKuan"
-    class="flex text-center font-semibold"
-  >
-    <text class="mr-3">应付金额:{{ orderDetail?.payMoney }}</text>
-    <!-- 倒计时组件 -->
-    <text>倒计时: </text>
-    <uni-countdown
-      :second="orderDetail?.countdown"
-      color="#000"
-      splitor-color="#000"
-      :show-day="false"
-      :show-colon="false"
-      @timeup="timeUp"
-    />
-  </view>
-
-  <view v-else class="flex justify-center font-semibold">
-    <view class="mr-3">
-      {{ orderStateList[orderDetail?.orderState]?.text }}
-    </view>
-    <navigator
-      class="button"
-      :url="`/orderPkg/editOrderPage/editOrderPage?orderId=${query.id}`"
-      hover-class="none"
-    >
-      再次购买
-    </navigator>
+    <!-- 账目 -->
     <view
-      v-if="orderDetail?.orderState == OrderState.DaiFaHuo"
-      class="button"
-      @tap="sendGoodsMock"
+      class="flex flex-col justify-around border-radius-primary bg-secondary margin-x-primary w-[363px] h-16 px-3"
     >
-      模拟发货
+      <view class="flex justify-between items-center">
+        <text class="font-secondary">商品总价</text>
+        <text class="font-secondary">${{ orderDetail?.totalMoney }}</text>
+      </view>
+      <view class="flex justify-between items-center">
+        <view class="flex items-center">
+          <text class="font-secondary">商品运费</text></view
+        >
+        <text class="font-secondary">${{ orderDetail?.postFee }}</text>
+      </view>
+      <view class="flex justify-between items-center">
+        <view class="flex items-center">
+          <text class="font-secondary">应付金额</text></view
+        >
+        <text class="font-secondary text-orange-800"
+          >${{ orderDetail?.payMoney }}</text
+        >
+      </view>
     </view>
-    <view
-      v-if="orderDetail?.orderState == OrderState.DaiShouHuo"
-      class="button"
-      @tap="confirmReceiveGoods"
-    >
-      确认收货
-    </view>
-  </view>
 
-  <!-- 取消 or 支付订单 -->
-  <view class="flex justify-between mx-1.5 text-white">
-    <button
-      class="bg-green-300 rounded-xl w-28 h-8 leading-8"
-      @tap="cancelOrder"
+    <!-- 支付剩余 -->
+    <view
+      v-if="orderDetail?.orderState === OrderState.DaiFuKuan"
+      class="flex text-center font-primary-smaller"
     >
-      取消订单
-    </button>
-    <button class="bg-red-300 rounded-xl w-36 h-8 leading-8" @tap="payOrder">
-      去支付
-    </button>
+      <text class="mr-3">应付金额:{{ orderDetail?.payMoney }}</text>
+      <!-- 倒计时组件 -->
+      <text>倒计时: </text>
+      <uni-countdown
+        :second="orderDetail?.countdown"
+        color="#000"
+        splitor-color="#000"
+        :show-day="false"
+        :show-colon="false"
+        @timeup="timeUp"
+      />
+    </view>
+
+    <view v-else class="flex justify-center">
+      <view class="mr-3 font-primary-smaller font-yellow">
+        {{ orderStateList[orderDetail?.orderState]?.text }}
+      </view>
+      <navigator
+        :url="`/orderPkg/editOrderPage/editOrderPage?orderId=${query.id}`"
+        hover-class="none"
+        class="font-primary-smaller"
+      >
+        再次购买
+      </navigator>
+      <view
+        v-if="orderDetail?.orderState == OrderState.DaiFaHuo"
+        class="button"
+        @tap="sendGoodsMock"
+      >
+        模拟发货
+      </view>
+      <view
+        v-if="orderDetail?.orderState == OrderState.DaiShouHuo"
+        class="button"
+        @tap="confirmReceiveGoods"
+      >
+        确认收货
+      </view>
+    </view>
+
+    <!-- 取消 or 支付订单 -->
+    <view class="flex justify-between margin-x-primary mb-3 text-white">
+      <button
+        class="bg-[#266972] border-radius-primary w-28 h-8 leading-8"
+        @tap="cancelOrder"
+      >
+        取消订单
+      </button>
+      <button
+        class="bg-orange-800 border-radius-primary w-36 h-8 leading-8"
+        @tap="payOrder"
+      >
+        去支付
+      </button>
+    </view>
   </view>
 </template>
 
@@ -155,6 +158,7 @@ import {
   getMemberOrderCancelByIdAPI,
 } from "./detailOrderPageApi";
 import { ref, onMounted } from "vue";
+import threeDots from "../../components/threeDots/threeDots.vue";
 
 // 获取页面参数
 const query = defineProps<{
