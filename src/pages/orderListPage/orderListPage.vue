5f4a866a435e1221911162393ca0b847731dae77
ziqing <1721370034@qq.com>
2024-02-25 21:40:58 +0800

    style(orderlistpage.vue): 标准化 订单列表页的前端界面设计

--- src/pages/orderListPage/orderListPage.vue
+++ src/pages/orderListPage/orderListPage.vue
@@ -1,70 +1,79 @@
 <template>
   <!-- tab -->
   <view
-    class="flex justify-between border-b border-slate-300 border-solid m-1.5 pb-1.5 w-[363px] h-20"
+    class="flex justify-between border-b border-slate-300 border-solid margin-x-primary my-1.5 pb-1.5 w-[363px] h-16"
   >
-    <view v-for="item in orderTabs" :key="item?.state">
-      <view>{{ item.title }}</view>
-      <uni-icons type="color" color="" size="24" />
+    <view
+      v-for="item in orderTabs"
+      :key="item?.state"
+      class="flex flex-col items-center justify-around"
+    >
+      <uni-icons :type="item.type" :color="item.color" size="20" />
+      <view class="font-primary">{{ item.title }}</view>
     </view>
   </view>
   <swiper :current="activeIndex" @change="activeIndex = $event.detail.current">
     <swiper-item v-for="tab in orderTabs" :key="tab?.state" class="h-96">
       <!-- 订单列表 -->
       <scroll-view scroll-y class="h-96">
-        <view v-for="order in orderList" :key="order.id" class="mx-1.5 mb-3">
-          <!-- 日期和订单状态 -->
-          <view class="flex justify-between w-[363px]">
-            <text class="text-xs text-slate-300">{{ order.createTime }}</text>
-            <text class="font-semibold text-sm">{{
-              orderStateList[order.orderState].text
-            }}</text>
-          </view>
-          <!-- 商品图片和信息 -->
-          <navigator
-            v-for="item in order.skus"
-            :key="item.id"
-            class="goods"
-            :url="`/pagesOrder/detail/detail?id=${order.id}`"
-            hover-class="none"
-          >
-            <view class="flex justify-start">
-              <image
-                src=""
-                mode="scaleToFill"
-                class="rounded-xl mr-3 h-16 w-16"
-              />
-              <view class="flex flex-col">
-                <text class="font-semibold">{{ item.name }}</text>
-                <text class="text-xs text-slate-300">商品规格</text>
-                <text class="text-xs text-slate-300"
-                  >共{{ order.totalNum }}件商品,实付:<text class="text-red-300">
-                    ${{ order.payMoney }}</text
-                  ></text
+        <tempalte v-if="orderList.length"
+          ><view v-for="order in orderList" :key="order.id" class="mx-1.5 mb-3">
+            <!-- 日期和订单状态 -->
+            <view class="flex justify-between w-[363px]">
+              <text class="text-xs text-slate-300">{{ order.createTime }}</text>
+              <text class="font-semibold text-sm">{{
+                orderStateList[order.orderState].text
+              }}</text>
+            </view>
+            <!-- 商品图片和信息 -->
+            <navigator
+              v-for="item in order.skus"
+              :key="item.id"
+              class="goods"
+              :url="`/pagesOrder/detail/detail?id=${order.id}`"
+              hover-class="none"
+            >
+              <view class="flex justify-start">
+                <image
+                  src=""
+                  mode="scaleToFill"
+                  class="rounded-xl mr-3 h-16 w-16"
+                />
+                <view class="flex flex-col">
+                  <text class="font-semibold">{{ item.name }}</text>
+                  <text class="text-xs text-slate-300">商品规格</text>
+                  <text class="text-xs text-slate-300"
+                    >共{{ order.totalNum }}件商品,实付:<text
+                      class="text-red-300"
+                    >
+                      ${{ order.payMoney }}</text
+                    ></text
+                  >
+                </view>
+              </view>
+              <!-- 按钮 -->
+              <view class="flex justify-end">
+                <button
+                  class="border border-slate-300 border-solid mr-3 w-20 h-8 leading-8"
+                >
+                  取消
+                </button>
+                <template v-if="order.orderState === OrderState.DaiFuKuan">
+                  <button
+                    class="border border-slate-600 border-solid w-20 h-8 leading-8"
+                  >
+                    去支付
+                  </button>
+                </template>
+
+                <template v-else
+                  ><button>再次购买</button> <button>确认收货</button></template
                 >
               </view>
-            </view>
-            <!-- 按钮 -->
-            <view class="flex justify-end">
-              <button
-                class="border border-slate-300 border-solid mr-3 w-20 h-8 leading-8"
-              >
-                取消
-              </button>
-              <template v-if="order.orderState === OrderState.DaiFuKuan">
-                <button
-                  class="border border-slate-600 border-solid w-20 h-8 leading-8"
-                >
-                  去支付
-                </button>
-              </template>
-
-              <template v-else
-                ><button>再次购买</button> <button>确认收货</button></template
-              >
-            </view>
-          </navigator>
-        </view>
+            </navigator>
+          </view></tempalte
+        >
+        <tempalte v-else>暂无订单</tempalte>
       </scroll-view>
     </swiper-item>
   </swiper>
@@ -83,11 +92,11 @@ const query = defineProps<{
 
 // tabs 数据
 const orderTabs = ref([
-  { state: 0, title: "全部" },
-  { state: 1, title: "待付款" },
-  { state: 2, title: "待发货" },
-  { state: 3, title: "待收货" },
-  { state: 4, title: "待评价" },
+  { state: 0, title: "全部", color: "#22c55e", type: "star-filled" },
+  { state: 1, title: "待付款", color: "#637ff1", type: "weixin" },
+  { state: 2, title: "待发货", color: "#fdba74", type: "cart-filled" },
+  { state: 3, title: "待收货", color: "#c2410c", type: "auth-filled" },
+  { state: 4, title: "待评价", color: "#f5c60b", type: "chatboxes-filled" },
 ]);
 
 // 高亮下标
--- src/pages/orderListPage/orderListPageApi.ts
+++ src/pages/orderListPage/orderListPageApi.ts
@@ -1,5 +1,5 @@
 import { http } from "../../utils/http";
-import type { OrderListResult } from "./orderListPageType";
+import type { OrderListResult, OrderListParams } from "./orderListPageType";
 
 export const getMemberOrderAPI = (data: OrderListParams) => {
   return http<OrderListResult>({
