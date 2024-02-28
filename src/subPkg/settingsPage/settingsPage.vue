28f1be9b458431d313e03a570df72175ebe45778
ziqing <1721370034@qq.com>
2024-02-28 14:03:14 +0800

    style(settingspage.vue): 完成设置页面的响应式设计
    [

--- src/subPkg/settingsPage/settingsPage.vue
+++ src/subPkg/settingsPage/settingsPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 我的收货地址 -->
-  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
+  <view class="flex items-center w-full px-1.5 my-1.5">
     <view
       class="border-b border-[#22c55e] border-solid border-radius-primary mr-1.5 mb-3 w-6 h-6 leading-[24px] text-center"
     >
@@ -14,7 +14,7 @@
   </view>
   <!-- 授权管理 -->
   <view
-    class="flex items-center border-b border-slate-300 border-solid w-[363px] margin-x-primary my-1.5 mb-7"
+    class="flex items-center border-b border-slate-300 border-solid w-full px-1.5 my-1.5 mb-7"
   >
     <view
       class="w-6 h-6 border-radius-primary mr-1.5 mb-3 text-center leading-[24px] border-b border-[#c2410c] border-solid w"
@@ -24,9 +24,7 @@
     <text class="ml-1.5 mb-3 font-primary-smaller">授权管理</text>
   </view>
   <!-- 问题反馈 -->
-  <view
-    class="flex items-center w-[363px] margin-x-primary my-1.5 font-semibold"
-  >
+  <view class="flex items-center w-full margin-x-primary my-1.5 font-semibold">
     <view
       class="w-6 h-6 6 border-radius-primary border-b border-[#F5C60B] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
@@ -35,16 +33,16 @@
     <text class="ml-1.5 mb-1.5 font-primary-smaller">问题反馈</text>
   </view>
   <!-- 联系我们 -->
-  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
+  <view class="flex items-center w-full margin-x-primary my-1.5">
     <view
       class="w-6 h-6 border-radius-primary border-b border-[#637FF1] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
       <i class="iconfont icon-a-042_biaoqing"></i>
     </view>
-    <text class="ml-1.5 mb-1.5 font-primary">联系我们</text>
+    <text class="ml-1.5 mb-1.5 font-primary-smaller">联系我们</text>
   </view>
   <!-- 关于本产品 -->
-  <view class="flex items-center w-[363px] margin-x-primary my-1.5">
+  <view class="flex items-center w-full margin-x-primary my-1.5">
     <view
       class="w-6 h-6 border-radius-primary border-b border-[#fdba74] border-solid mr-1.5 mb-1.5 text-center leading-[24px]"
     >
@@ -53,7 +51,7 @@
     <text class="ml-1.5 mb-1.5 font-primary-smaller">关于本产品</text>
   </view>
   <button
-    class="bg-[#9A3412] border-radius-primary m-1.5 mt-9 w-[363px] h-12 leading-[48px] font-base font-semibold text-white"
+    class="bg-[#9A3412] border-radius-primary mt-9 mx-auto px-6 py-3 w-fit font-base font-semibold text-white"
   >
     退出登录
   </button>
