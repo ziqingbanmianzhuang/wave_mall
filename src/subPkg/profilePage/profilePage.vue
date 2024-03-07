cec7bbe696e02dbcd8d605a9601da5fefef54e55
ziqing <1721370034@qq.com>
2024-03-07 23:52:21 +0800

    feat(profilepage.vue): 完成个人详情页的加载逻辑

--- src/subPkg/profilePage/profilePage.vue
+++ src/subPkg/profilePage/profilePage.vue
@@ -1,153 +1,157 @@
 <template>
-  <view v-if="profileStore.profile" class="bg-primary w-full h-container">
-    <!-- 背景图 -->
-    <view class="bg-slate-200 w-full h-48 min-[440px]:h-32 min-[960px]:h-72">
-      <image
-        :src="profile?.avatar"
-        mode="aspectFill"
-        class="w-full h-48 min-[440px]:h-32 min-[960px]:h-72"
-        @tap="onAvatarChange"
-      />
-    </view>
-
-    <view
-      class="relative top-[-30px] left-0 bg-white border-radius-primary mx-1.5"
-    >
-      <!-- 头像昵称 -->
-      <view
-        class="relative top-[-40px] left-1/2 -translate-x-1/2 flex flex-col justify-center items-center text-center"
-      >
+  <view v-show="!isLoading">
+    <view v-if="profileStore.profile" class="bg-primary w-full h-container">
+      <!-- 背景图 -->
+      <view class="bg-slate-200 w-full h-48 min-[440px]:h-32 min-[960px]:h-72">
         <image
           :src="profile?.avatar"
           mode="aspectFill"
-          class="border-radius-primary h-12 w-12 min-[960px]:h-32 min-[960px]:w-32"
+          class="w-full h-48 min-[440px]:h-32 min-[960px]:h-72"
           @tap="onAvatarChange"
         />
-        <text
-          class="block border-b border-gray-300 border-solid mt-3 w-full font-primary"
-          >{{ profile?.account }}</text
-        >
       </view>
-      <!-- 表单 -->
-      <view
-        class="grid grid-cols-5 auto-rows-[50px] min-[960px]:grid-cols-6 justify-items-end px-1.5"
-      >
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >账号</text
-        >
-        <text class="font-primary-smaller col-span-3 justify-self-start"
-          >waveMall</text
-        >
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >昵称</text
-        >
-        <input
-          v-model="profile.nickname"
-          type="text"
-          placeholder="请填写昵称"
-          placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
-          class="input-primary col-span-3 justify-self-start"
-        />
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >性别</text
-        >
-        <radio-group
-          class="col-span-3 justify-self-start"
-          @change="onGenderChange"
-        >
-          <label>
-            <radio
-              value="男"
-              color="#27ba9b"
-              :checked="profile?.gender === '男'"
-              style="transform: scale(0.7)"
-            />
-            <text class="font-primary-smaller mr-3">男</text>
-          </label>
-          <label>
-            <radio
-              value="女"
-              color="#27ba9b"
-              :checked="profile?.gender === '女'"
-              style="transform: scale(0.7)"
-            />
-            <text class="font-primary-smaller">女</text>
-          </label>
-        </radio-group>
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >生日</text
-        >
-        <picker
-          mode="date"
-          :value="profile?.birthday"
-          start="1900-01-01"
-          :end="new Date()"
-          class="input-primary col-span-3 justify-self-start"
-          @change="onBirthdayChange"
-        >
-          <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
-          <view v-else class="font-primary-smaller input-secondary"
-            >请选择日期</view
-          >
-        </picker>
 
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >城市</text
-        >
-        <picker
-          class="picker input-primary col-span-3 justify-self-start"
-          :value="profile?.fullLocation?.split(' ')"
-          mode="region"
-          @change="onFullLocationChange"
-        >
-          <view v-if="profile?.fullLocation">{{ profile.fullLocation }}</view>
-          <view v-else class="font-primary-smaller input-secondary"
-            >请选择城市</view
-          >
-        </picker>
-        <text
-          class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
-          >职业</text
-        >
-        <input
-          v-model="profile.profession"
-          type="text"
-          placeholder="请填写职业"
-          class="input-primary col-span-3 justify-self-start"
-          placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
-        />
-      </view>
-      <view class="flex mb-3 justify-center">
-        <button
-          class="bg-[#9A3412] border-radius-primary py-3 px-6 text-white font-sm font-semibold"
-          @tap="onSubmit"
-        >
-          保存
-        </button>
-      </view>
-    </view>
-  </view>
-  <view v-else>
-    <view class="w-full text-center">
-      <text class="font-primary">你还没有登录嗷</text>
-      <navigator
-        url="/pages/loginPage/loginPage"
-        open-type="navigate"
-        hover-class="navigator-hover"
+      <view
+        class="relative top-[-30px] left-0 bg-white border-radius-primary mx-1.5"
       >
-        <button
-          class="bg-[#9A3412] border-radius-primary mx-auto my-3 px-6 py-3 w-fit font-primary-smaller text-white font-semibold"
+        <!-- 头像昵称 -->
+        <view
+          class="relative top-[-40px] left-1/2 -translate-x-1/2 flex flex-col justify-center items-center text-center"
         >
-          去登录
-        </button>
-      </navigator>
+          <image
+            :src="profile?.avatar"
+            mode="aspectFill"
+            class="border-radius-primary h-12 w-12 min-[960px]:h-32 min-[960px]:w-32"
+            @tap="onAvatarChange"
+          />
+          <text
+            class="block border-b border-gray-300 border-solid mt-3 w-full font-primary"
+            >{{ profile?.account }}</text
+          >
+        </view>
+        <!-- 表单 -->
+        <view
+          class="grid grid-cols-5 auto-rows-[50px] min-[960px]:grid-cols-6 justify-items-end px-1.5"
+        >
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >账号</text
+          >
+          <text class="font-primary-smaller col-span-3 justify-self-start"
+            >waveMall</text
+          >
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >昵称</text
+          >
+          <input
+            v-model="profile.nickname"
+            type="text"
+            placeholder="请填写昵称"
+            placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
+            class="input-primary col-span-3 justify-self-start"
+          />
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >性别</text
+          >
+          <radio-group
+            class="col-span-3 justify-self-start"
+            @change="onGenderChange"
+          >
+            <label>
+              <radio
+                value="男"
+                color="#27ba9b"
+                :checked="profile?.gender === '男'"
+                style="transform: scale(0.7)"
+              />
+              <text class="font-primary-smaller mr-3">男</text>
+            </label>
+            <label>
+              <radio
+                value="女"
+                color="#27ba9b"
+                :checked="profile?.gender === '女'"
+                style="transform: scale(0.7)"
+              />
+              <text class="font-primary-smaller">女</text>
+            </label>
+          </radio-group>
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >生日</text
+          >
+          <picker
+            mode="date"
+            :value="profile?.birthday"
+            start="1900-01-01"
+            :end="new Date()"
+            class="input-primary col-span-3 justify-self-start"
+            @change="onBirthdayChange"
+          >
+            <view v-if="profile?.birthday">{{ profile?.birthday }}</view>
+            <view v-else class="font-primary-smaller input-secondary"
+              >请选择日期</view
+            >
+          </picker>
+
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >城市</text
+          >
+          <picker
+            class="picker input-primary col-span-3 justify-self-start"
+            :value="profile?.fullLocation?.split(' ')"
+            mode="region"
+            @change="onFullLocationChange"
+          >
+            <view v-if="profile?.fullLocation">{{ profile.fullLocation }}</view>
+            <view v-else class="font-primary-smaller input-secondary"
+              >请选择城市</view
+            >
+          </picker>
+          <text
+            class="mr-20 font-primary-smaller col-span-2 min-[960px]:col-span-3 text-nowrap"
+            >职业</text
+          >
+          <input
+            v-model="profile.profession"
+            type="text"
+            placeholder="请填写职业"
+            class="input-primary col-span-3 justify-self-start"
+            placeholder-style="color:#b4b5c5;font-size:12px;font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;  font-weight: 600"
+          />
+        </view>
+        <view class="flex mb-3 justify-center">
+          <button
+            class="bg-[#9A3412] border-radius-primary py-3 px-6 text-white font-sm font-semibold"
+            @tap="onSubmit"
+          >
+            保存
+          </button>
+        </view>
+      </view>
+    </view>
+    <view v-else>
+      <view class="w-full text-center">
+        <text class="font-primary">你还没有登录嗷</text>
+        <navigator
+          url="/pages/loginPage/loginPage"
+          open-type="navigate"
+          hover-class="navigator-hover"
+        >
+          <button
+            class="bg-[#9A3412] border-radius-primary mx-auto my-3 px-6 py-3 w-fit font-primary-smaller text-white font-semibold"
+          >
+            去登录
+          </button>
+        </navigator>
+      </view>
     </view>
   </view>
+
+  <doubleCircleLoading v-show="isLoading"></doubleCircleLoading>
 </template>
 
 <script lang="ts" setup>
@@ -156,6 +160,10 @@ import type { ProfileDetail, Gender, ProfileParams } from "./profileType";
 import { ref, onMounted } from "vue";
 import { useProfileStore } from "../../store/profile/index";
 import { onShow } from "@dcloudio/uni-app";
+
+import doubleCircleLoading from "../../components/doubleCirlcleLoading/doubleCircleLoading.vue";
+import { globalLoadingHook } from "../../hooks/globalLoadingHook";
+const { isLoading, setLoading } = globalLoadingHook();
 
 // 获取会员Store
 const profileStore = useProfileStore();
@@ -172,8 +180,10 @@ const profile = ref({
   profession: "",
 } as ProfileDetail);
 const getMemberProfileData = async () => {
+  setLoading(true);
   const res = await getMemberProfileAPI();
   profile.value = res.result;
+  setLoading(false);
 };
 
 //修改性别
