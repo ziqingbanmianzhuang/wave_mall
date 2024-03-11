1cbcbb380ed72776d5e405bd146296179189d2d4
ziqing <1721370034@qq.com>
2024-03-11 11:14:41 +0800

    feat(vite.config.ts,manifest.jsln): 修改h5打包配置,统一修改基础路径为./,针对打包的环境条件性的开启weapp-tailwind插件的应用
    条件应用weapp-tailwind解决了H5端打包后样式全部出错的问题

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,19 @@ import autoprefixer from "autoprefixer";
 import { UnifiedViteWeappTailwindcssPlugin as uvwt } from "weapp-tailwindcss/vite";
 import { UnifiedViteWeappTailwindcssPlugin } from "weapp-tailwindcss/vite";
 
+const isH5 = process.env.UNI_PLATFORM === "h5";
+// vue2 版本为 app-plus
+const isApp = process.env.UNI_PLATFORM === "app-plus";
+const WeappTailwindcssDisabled = isH5 || isApp;
+// vue3 版本构建到 app, UNI_PLATFORM 的值为 app
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
     uvwt(),
     UnifiedViteWeappTailwindcssPlugin({
+      disabled: WeappTailwindcssDisabled,
       rem2rpx: true,
     }),
     VueDevTools(),
